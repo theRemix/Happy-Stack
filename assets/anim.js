2558cbb811444a37f66908e4acfdba5cdf289cb2
Jon Borgonia <jon@gomagames.com>
2018-06-15 01:18:25 -0700

    add monster animation

--- assets/anim.js
+++ assets/anim.js
@@ -1,5 +1,6 @@
 (function(window, document) {
   var animating = false;
+  var monster = document.querySelector('.monster-pop');
   var distance = document.querySelector('.distance');
   var label = document.querySelector('.distance-label');
   var moonDistance = 238900;
@@ -12,13 +13,20 @@
     animating = false;
   });
 
+  function format(float){
+    return float.toFixed().toString().split('').reduce(function(number, digit, i, a){
+      return number + ( (a.length-1 - i % 3 === 3 && i < a.length-1) ? digit + "," : digit );
+    }, '');
+  }
+
   function animate(timestamp) {
     if(animating){
-      var stepDistance = ( distance.clientWidth / ( .46 * document.body.clientWidth ) * moonDistance );
-      label.innerText = stepDistance > moonDistance / 1.5 ? stepDistance.toFixed() + " mi" : stepDistance.toFixed();
+      var stepDistance = ( distance.clientWidth / ( .52 * document.body.clientWidth ) * moonDistance );
+      label.innerText = stepDistance > moonDistance / 2 ? format(stepDistance) + " mi" : format(stepDistance);
       window.requestAnimationFrame(animate);
     } else {
-      label.innerText = moonDistance + " mi";
+      label.innerText = format(moonDistance) + " mi";
+      monster.classList.add('monster-pop-up');
     }
   }
 
